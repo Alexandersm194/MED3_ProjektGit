099e1a2d5cde8569b4fe7e4fa0a5b1d25deae4c0
pskrub <paulskrubbeltrang@gmail.com>
2025-10-08 14:06:36 +0200

    did more work on rectify

--- RectifyTest.py
+++ RectifyTest.py
@@ -2,25 +2,55 @@ import cv2 as cv
 import numpy as np
 
 chessIMG = cv.imread('TrainingImages/chessboard.jpg')
-chessIMG = cv.resize(chessIMG,(640,480))
+chessIMG = cv.resize(chessIMG,(0,0), fx=0.5, fy=0.5)
+chessIMGGray = cv.cvtColor(chessIMG, cv.COLOR_BGR2GRAY)
+
+finalWidth = 600
+finalHeight = 600
+
+#initialize lowest x and y value for corners
+xMin, xMax, yMin, yMax = 1000, 0, 1000, 0
+
+#draw cicles at coordinates
+# cv.circle(chessIMG, tl, 5, (0,0,255), 2)
+# cv.circle(chessIMG, bl, 5, (0,0,255), 2)
+# cv.circle(chessIMG, tr, 5, (0,0,255), 2)
+# cv.circle(chessIMG, br, 5, (0,0,255), 2)
+
+
+#goodFeaturesToTrack detects corners
+corners = cv.goodFeaturesToTrack(chessIMGGray, 100, 0.12, 10)
+corners = np.intp(corners)
+
+for corner in corners:
+    x, y = corner.ravel() #ravel flattens the array
+    if x < xMin:
+        xMin = x
+    if x > xMax:
+        xMax = x
+    if y < yMin:
+        yMin = y
+    if y > yMax:
+        yMax = y
+    cv.circle(chessIMG, (x, y), 5, (0,0,255), 0)
+    # print(x, y)
+
+print(xMin, xMax, yMin, yMax)
+
 
 #select coordinates
-tl = (270, 50)
-bl = (80, 240)
-tr = (555, 140)
-br = (410, 380)
+tl = (540, 95)
+bl = (165, 455)
+tr = (1113, 265)
+br = (825, 715)
 
-cv.circle(chessIMG, tl, 5, (0,0,255), 2)
-cv.circle(chessIMG, bl, 5, (0,0,255), 2)
-cv.circle(chessIMG, tr, 5, (0,0,255), 2)
-cv.circle(chessIMG, br, 5, (0,0,255), 2)
 
 #apply transformation
 pointsOriginal = np.float32([tl, bl, tr, br])
-pointsTransformed = np.float32([(0,0), (0,480), (640,0), (640,480)])
+pointsTransformed = np.float32([(0,0), (0,finalHeight), (finalWidth,0), (finalWidth,finalHeight)])
 
 matrix = cv.getPerspectiveTransform(pointsOriginal, pointsTransformed)
-chessTransformed = cv.warpPerspective(chessIMG, matrix, (640, 480))
+chessTransformed = cv.warpPerspective(chessIMG, matrix, (finalWidth, finalHeight))
 
 cv.imshow('Original', chessIMG)
 cv.imshow('Transformed', chessTransformed)
