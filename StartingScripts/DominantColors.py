72331801be81f7b10ea3de514f61b604d208582b
pskrub <paulskrubbeltrang@gmail.com>
2025-11-12 13:34:13 +0100

    changed kmeans from random to pp

--- StartingScripts/DominantColors.py
+++ StartingScripts/DominantColors.py
@@ -7,19 +7,19 @@ def createBars(height, width, color):
     red, green, blue = int(color[2]), int(color[1]), int(color[0]) #correct from bgr to rgb
     return bar, (red, green, blue)
 
-img = cv.imread('C:/Users/pauls/PycharmProjects/MED3_ProjektGit/fishImages/fish1/fish1_1.jpg')
-img = cv.resize(img,(1008,756))
-img = img[270:500,220:800]
+img = cv.imread('C:/Users/pauls/PycharmProjects/MED3_ProjektGit/fishImages/CoppedImages/image.png')
+# img = cv.resize(img,(1008,756))
+# img = img[270:500,220:800]
 height, width, _ = np.shape(img)
 
 data = np.reshape(img,(height * width, 3)) #get all pixel values into a list
 data = np.float32(data) #convert to float
 
-clusterAmount = 8
+clusterAmount = 4
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 15, 2.0) #maximum iterations and desired accuracy for criteria
-flags = cv.KMEANS_RANDOM_CENTERS
-compactness, labels, centers = cv.kmeans(data, clusterAmount, None, criteria, 10, flags=flags) #centers of where the dominant colors are
-# print(centers)
+flags = cv.KMEANS_PP_CENTERS
+compactness, labels, centers = cv.kmeans(data, clusterAmount, None, criteria, 10, flags=flags)
+print(centers)
 
 font = cv.FONT_HERSHEY_SIMPLEX
 bars = []
@@ -30,6 +30,8 @@ for index, row in enumerate(centers):
     bars.append(bar)
     rgbValues.append(rgb)
 
+#could try to merge colors with similar rgb values
+#if rgbValues[i] > rgbValues[0]+30 or rgbValues[i] < rgbValues[0]-30
 imgBar = np.hstack(bars)
 
 for index, row in enumerate(rgbValues):
