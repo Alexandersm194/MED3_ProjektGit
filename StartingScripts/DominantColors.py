3fcaeef4b3235913f2848361debc60a96b53f203
pskrub <paulskrubbeltrang@gmail.com>
2025-11-12 12:31:51 +0100

    added script for detecting dominant colors

---
+++ StartingScripts/DominantColors.py
@@ -0,0 +1,42 @@
+import cv2 as cv
+import numpy as np
+
+def createBars(height, width, color):
+    bar = np.zeros((height, width, 3), np.uint8)
+    bar [:] = color
+    red, green, blue = int(color[2]), int(color[1]), int(color[0]) #correct from bgr to rgb
+    return bar, (red, green, blue)
+
+img = cv.imread('C:/Users/pauls/PycharmProjects/MED3_ProjektGit/fishImages/fish1/fish1_1.jpg')
+img = cv.resize(img,(1008,756))
+img = img[270:500,220:800]
+height, width, _ = np.shape(img)
+
+data = np.reshape(img,(height * width, 3)) #get all pixel values into a list
+data = np.float32(data) #convert to float
+
+clusterAmount = 8
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 15, 2.0) #maximum iterations and desired accuracy for criteria
+flags = cv.KMEANS_RANDOM_CENTERS
+compactness, labels, centers = cv.kmeans(data, clusterAmount, None, criteria, 10, flags=flags) #centers of where the dominant colors are
+# print(centers)
+
+font = cv.FONT_HERSHEY_SIMPLEX
+bars = []
+rgbValues = []
+
+for index, row in enumerate(centers):
+    bar, rgb = createBars(200, 200, row)
+    bars.append(bar)
+    rgbValues.append(rgb)
+
+imgBar = np.hstack(bars)
+
+for index, row in enumerate(rgbValues):
+    image = cv.putText(imgBar, f'RGB: {row}', (5 + 200 * index, 200 - 10), font, 0.5, (0, 0, 0), 1, cv.LINE_AA)
+    # print(f'{index + 1}. RGB: {row}')
+
+cv.imshow('Dominant colors', imgBar)
+cv.imshow('Image', img)
+
+cv.waitKey(0)
