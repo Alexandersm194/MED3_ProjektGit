a9266223251bc3477d1012d6a4927124e837147a
pskrub <paulskrubbeltrang@gmail.com>
2025-11-13 10:05:27 +0100

    removed ReduceColors script and moved the coed into DominantColors

--- StartingScripts/DominantColors.py
+++ StartingScripts/DominantColors.py
@@ -7,15 +7,15 @@ def createBars(height, width, color):
     red, green, blue = int(color[2]), int(color[1]), int(color[0]) #correct from bgr to rgb
     return bar, (red, green, blue)
 
-img = cv.imread('C:/Users/pauls/PycharmProjects/MED3_ProjektGit/fishImages/CoppedImages/image.png')
-# img = cv.resize(img,(1008,756))
-# img = img[270:500,220:800]
+img = cv.imread('C:/Users/pauls/PycharmProjects/MED3_ProjektGit/fishImages/fish1/fish1_1.jpg')
+img = cv.resize(img,(1008,756))
+img = img[270:500,220:800]
 height, width, _ = np.shape(img)
 
 data = np.reshape(img,(height * width, 3)) #get all pixel values into a list
 data = np.float32(data) #convert to float
 
-clusterAmount = 6
+clusterAmount = 7
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 15, 2.0) #maximum iterations and desired accuracy for criteria
 flags = cv.KMEANS_PP_CENTERS
 compactness, labels, centers = cv.kmeans(data, clusterAmount, None, criteria, 10, flags=flags)
@@ -38,7 +38,13 @@ for index, row in enumerate(rgbValues):
     image = cv.putText(imgBar, f'RGB: {row}', (5 + 200 * index, 200 - 10), font, 0.5, (0, 0, 0), 1, cv.LINE_AA)
     # print(f'{index + 1}. RGB: {row}')
 
+#Quantize image
+centers = np.uint8(centers)
+imgQuantized = centers[labels.flatten()]
+imgQuantized = imgQuantized.reshape(img.shape)
+
 cv.imshow('Dominant colors', imgBar)
+cv.imshow('Image colors quantized', imgQuantized)
 cv.imshow('Image', img)
 
 cv.waitKey(0)
